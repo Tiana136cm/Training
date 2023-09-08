fcf23410625a5c4be9b83097da5927b29f0d22c0
Tiana <129862083+Tiana136cm@users.noreply.github.com>
2023-09-08 15:51:23 +0800

    Update D.py

--- 111模擬賽/D.py
+++ 111模擬賽/D.py
@@ -6,14 +6,12 @@ for _ in range(int(input().strip())):
         print(0)
         continue
 
-    k = n // 2  # 在左邊的node數
-    # 左邊的樹的排列對到一個獨一無二的右邊樹的排列: 對射(bijection)
+    k = n // 2
 
     if len(memo) > k:
         print(memo[k])
 
     for i in range(len(memo), k + 1):
-        # 排列組合: 左邊的node亂排 * 右邊的node亂排
         memo.append(sum(memo[j] * memo[i - j - 1] for j in range(i)))
 
     print(memo[k])
