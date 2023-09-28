96d5d4131f1903d48eaf2bac8a5f7ae1762433e7
Tiana <129862083+Tiana136cm@users.noreply.github.com>
2023-09-28 18:01:02 +0800

    Fixed logic and added comments to C1.py

--- 111模擬賽/C1.py
+++ 111模擬賽/C1.py
@@ -2,8 +2,9 @@ from math import ceil, sqrt
 
 
 def is_prime(k):
-    a = int(ceil(sqrt(k)))
-    return not any(k % i == 0 for i in range(2, a + 1))
+    # all(k % i != 0 for i in range(2, int(ceil(sqrt(k))) + 1))
+    # but all number that is not equal to 0 is true, thus `!= 0` can be omitted
+    return k == 2 or all(k % i for i in range(2, int(ceil(sqrt(k))) + 1))
 
 
 for k in map(int, iter(input, '0')):
